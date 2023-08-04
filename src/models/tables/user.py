814b523d1675b84c2dbffc6d88d8d825caa3104f
JKearnsl <boyarshinovn@gmail.com>
2023-08-04 15:13:10 +0300

    Добавлена модель таблицы `User`

--- src/models/tables/user.py
+++ src/models/tables/user.py
@@ -1,7 +1,6 @@
 import uuid
 
 from sqlalchemy import Column, UUID, VARCHAR, INTEGER, Enum, DateTime, func
-from sqlalchemy.orm import relationship
 
 from src.models.role import Role, MainRole as M, AdditionalRole as A
 from src.models.state import UserState
@@ -24,10 +23,10 @@ class User(Base):
     state = Column(Enum(UserState), default=UserState.NOT_CONFIRMED)
     hashed_password = Column(VARCHAR(255))
 
-    articles = relationship("models.tables.article.Article", back_populates="owner")
-    comments = relationship("models.tables.comment.Comment", back_populates="owner")
-    notifications = relationship("models.tables.notification.Notification", back_populates="owner")
-    files = relationship("models.tables.file.File", back_populates="owner")
+    # articles = relationship("models.tables.article.Article", back_populates="owner")
+    # comments = relationship("models.tables.comment.Comment", back_populates="owner")
+    # notifications = relationship("models.tables.notification.Notification", back_populates="owner")
+    # files = relationship("models.tables.file.File", back_populates="owner")
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
