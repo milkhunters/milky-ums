17dc2d9a2ae49563c1299685126f5b517584d012
JKearnsl <boyarshinovn@gmail.com>
2023-08-27 11:25:32 +0300

    Добавлены схемы связанные с ролями

--- src/models/schemas/__init__.py
+++ src/models/schemas/__init__.py
@@ -8,5 +8,11 @@ from .user import UserUpdateByAdmin
 from .user import UserCreate
 from .user import UserAuth
 
+from .role import Role
+from .role import Access
+from .role import UpdateRole
+from .role import CreateRole
+from .role import CreateAccess
+
 from .error import Error
 from .error import FieldErrorItem
