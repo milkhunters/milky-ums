8240b59995d953ce2bd25add6ea67ea8b746b026
JKearnsl <boyarshinovn@gmail.com>
2023-08-27 11:28:04 +0300

    Добавлены роли

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -2,3 +2,7 @@ from .base import BaseView
 
 from .user import UserResponse
 from .user import UserSmallResponse
+from .role import RoleResponse
+from .role import RolesResponse
+from .role import AccessResponse
+from .role import AccessesResponse
