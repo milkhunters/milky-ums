afd88ad989ea046dbae10fc9fe045c0f6399b73f
JKearnsl <boyarshinovn@gmail.com>
2023-08-27 11:29:36 +0300

    Добавлен маршрут ролей в главный роутер

--- src/router.py
+++ src/router.py
@@ -3,13 +3,14 @@ from fastapi import APIRouter
 from src.controllers import auth
 from src.controllers import user
 from src.controllers import stats
-
+from src.controllers import role
 
 def register_api_router(is_debug: bool) -> APIRouter:
     root_api_router = APIRouter(prefix="/api/v1" if is_debug else "")
 
     root_api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
     root_api_router.include_router(user.router, prefix="/user", tags=["User"])
+    root_api_router.include_router(role.router, prefix="/role", tags=["Role"])
     root_api_router.include_router(stats.router, prefix="", tags=["Stats"])
 
     return root_api_router
