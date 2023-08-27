5bd3862a46e75c273a8fcb1411704c637fef3eb7
JKearnsl <boyarshinovn@gmail.com>
2023-08-27 11:25:25 +0300

    Добавлены схемы связанные с ролями

---
+++ src/models/schemas/role.py
@@ -0,0 +1,37 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Access(BaseModel):
+    id: uuid.UUID
+    title: str
+    created_at: datetime
+    updated_at: datetime | None
+
+    class Config:
+        from_attributes = True
+
+
+class Role(BaseModel):
+    id: uuid.UUID
+    title: str
+    access: list[Access] | None
+    created_at: datetime
+    updated_at: datetime | None
+
+    class Config:
+        from_attributes = True
+
+
+class UpdateRole(BaseModel):
+    title: str
+
+
+class CreateRole(BaseModel):
+    title: str
+
+
+class CreateAccess(BaseModel):
+    title: str
