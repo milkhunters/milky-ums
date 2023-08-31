36265a90c40ff0077a2324e9dfc07fc3e7c1525a
JKearnsl <boyarshinovn@gmail.com>
2023-08-31 10:23:55 +0300

    Изменено наименование

--- src/dependencies/services.py
+++ src/dependencies/services.py
@@ -14,7 +14,7 @@ async def get_services(request: Request, repos: RepoFactory = Depends(get_repos)
         repos,
         current_user=local_scope.get("user"),
         redis_client=global_scope.redis,
-        redis_client_for_kick_list=global_scope.redis_for_kick_list,
+        redis_client_reauth=global_scope.redis_client_reauth,
         config=global_scope.config,
         email_sender=global_scope.email_sender,
     )
