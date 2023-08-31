dc9ee6201f81461a1314c6cb288d8a11d4bd1734
JKearnsl <boyarshinovn@gmail.com>
2023-08-31 10:17:50 +0300

    Добавлены маршруты управления сессией авторизации

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -8,6 +8,7 @@ from fastapi.responses import Response
 from src.dependencies.services import get_services
 from src.models import schemas
 from src.services import ServiceFactory
+from src.views import SessionsResponse
 
 from src.views.user import UserResponse, UserSmallResponse
 
@@ -93,3 +94,51 @@ async def delete_current_user(
     """
     await services.user.delete_me(password)
     await services.auth.logout(request, response)
+
+
+@router.get("/session/list", response_model=SessionsResponse, status_code=http_status.HTTP_200_OK)
+async def get_self_sessions(services: ServiceFactory = Depends(get_services)):
+    """
+    Получить список сессий текущего пользователя
+
+    Требуемые права доступа: CAN_GET_SELF_SESSIONS
+
+    Состояние: ACTIVE
+    """
+    return SessionsResponse(content=await services.user.get_my_sessions())
+
+
+@router.get("/session/list/{user_id}", response_model=SessionsResponse, status_code=http_status.HTTP_200_OK)
+async def get_user_sessions(user_id: uuid.UUID, services: ServiceFactory = Depends(get_services)):
+    """
+    Получить список сессий пользователя по id
+
+    Требуемые права доступа: CAN_GET_USER_SESSIONS
+
+    Состояние: ACTIVE
+    """
+    return SessionsResponse(content=await services.user.get_user_sessions(user_id))
+
+
+@router.delete("/session/delete", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
+async def delete_current_session(session_id: str, services: ServiceFactory = Depends(get_services)):
+    """
+    Удалить свою сессию по id
+
+    Требуемые права доступа: CAN_DELETE_SELF_SESSION
+
+    Состояние: ACTIVE
+    """
+    await services.user.delete_my_session(session_id)
+
+
+@router.delete("/session/delete/{user_id}", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
+async def delete_user_session(user_id: uuid.UUID, session_id: str, services: ServiceFactory = Depends(get_services)):
+    """
+    Удалить сессию пользователя по id
+
+    Требуемые права доступа: CAN_DELETE_USER_SESSION
+
+    Состояние: ACTIVE
+    """
+    await services.user.delete_user_session(user_id, session_id)
