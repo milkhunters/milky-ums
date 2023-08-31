22eb9a0f0f3e1654f6f8c81f0ff1e7889d986db1
JKearnsl <boyarshinovn@gmail.com>
2023-08-31 10:22:29 +0300

    Переработана модель хранения пользовательских сессий, добавлено больше функций управления

--- src/services/auth/session.py
+++ src/services/auth/session.py
@@ -1,3 +1,4 @@
+import time
 import uuid
 
 from fastapi import Request, Response
@@ -29,8 +30,11 @@ class SessionManager:
     async def set_session_id(
             self,
             response: Response,
+            user_id: uuid.UUID,
             refresh_token: str,
-            session_id: str = None
+            ip_address: str,
+            user_agent: str,
+            session_id: str = None,
     ) -> str:
         """
         Генерирует (если не передано) и устанавливает сессию в redis и в куки
@@ -38,6 +42,9 @@ class SessionManager:
         :param response:
         :param refresh_token:
         :param session_id:
+        :param user_id:
+        :param ip_address:
+        :param user_agent:
         :return: session_id
         """
         if not session_id:
@@ -51,35 +58,74 @@ class SessionManager:
             max_age=self._config.JWT.REFRESH_EXPIRE_SECONDS,
             path=self.COOKIE_PATH
         )
-        await self._redis_client.set(session_id, refresh_token, expire=self._config.JWT.REFRESH_EXPIRE_SECONDS)
+        data = f"{refresh_token}:{ip_address}:{int(time.time())}:{user_agent}"
+        await self._redis_client.hset(f'session_mapping:{user_id}', session_id, data)
+        await self._redis_client.expire(f'session_mapping:{user_id}', 15_638_400)
         return session_id
 
-    async def delete_session_id(self, session_id: str, response: Response) -> None:
+    async def get_user_sessions(self, user_id: uuid.UUID) -> dict[str, dict[str, str]]:
+        records = await self._redis_client.hgetall(f'session_mapping:{user_id}')
+        response = dict()
+        for key, value in records.items():
+            data = value.split(":", 4)
+            response[key] = dict(
+                refresh_token=data[0],
+                ip=data[1],
+                time=int(data[2]),
+                user_agent=data[3],
+            )
+        return response
+
+    async def delete_session(self, user_id, session_id: str, response: Response = None) -> None:
         """
         Удаляет сессию из куков и из redis
 
+        :param user_id:
         :param session_id
         :param response
         """
-        await self._redis_client.delete(session_id)
-        response.delete_cookie(
-            key=self.COOKIE_SESSION_KEY,
-            secure=True,
-            httponly=True,
-            samesite="none",
-            path=self.COOKIE_PATH
+        if await self._redis_client.hexists(f'session_mapping:{user_id}', session_id):
+            await self._redis_client.hdel(f'session_mapping:{user_id}', session_id)
+        if response:
+            response.delete_cookie(
+                key=self.COOKIE_SESSION_KEY,
+                secure=True,
+                httponly=True,
+                samesite="none",
+                path=self.COOKIE_PATH
+            )
+
+    async def get_data_from_session(self, user_id: str, session_id: str) -> dict[str, str] | None:
+        """
+        Получает данные сессии из session
+
+        :param user_id:
+        :param session_id:
+        :return: dict
+        """
+        data = await self._redis_client.hget(f'session_mapping:{user_id}', session_id)
+        if not data:
+            return None
+        data = data.split(":", 4)
+        return dict(
+            refresh_token=data[0],
+            ip=data[1],
+            time=data[2],
+            user_agent=data[3],
         )
 
-    async def is_valid_session(self, session_id: str, cookie_refresh_token: str) -> bool:
+    async def is_valid_session(self, user_id: str, session_id: str, refresh_token: str) -> bool:
         """
         Проверяет валидность сессии
+
+        :param user_id:
         :param session_id:
-        :param cookie_refresh_token:
+        :param refresh_token:
         :return: True or False
         """
-        refresh_token_from_redis = await self._redis_client.get(session_id)
-        if not refresh_token_from_redis:
+        data = await self.get_data_from_session(user_id, session_id)
+        if not data:
             return False
-        if refresh_token_from_redis != cookie_refresh_token:
+        if data["refresh_token"] != refresh_token:
             return False
         return True
