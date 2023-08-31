47968041f959bd0d995fff804c23a990814baa77
JKearnsl <boyarshinovn@gmail.com>
2023-08-31 10:18:30 +0300

    Обновлена пользовательская выходная модель

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -2,7 +2,10 @@ from .base import BaseView
 
 from .user import UserResponse
 from .user import UserSmallResponse
+
 from .role import RoleResponse
 from .role import RolesResponse
 from .role import AccessResponse
 from .role import AccessesResponse
+
+from .session import SessionsResponse
