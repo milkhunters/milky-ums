2c12e821d67427af31a11fa7cd93af21042378fb
JKearnsl <boyarshinovn@gmail.com>
2023-09-22 14:44:19 +0300

    Обновлен паттерн валидации пароля

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -1,16 +1,11 @@
 import re
 
+
 def is_valid_username(username: str) -> bool:
     pattern = r"^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
-    if re.match(pattern, username) is not None:
-        return True
-    else:
-        return False
+    return re.match(pattern, username) is not None
 
 
 def is_valid_password(password: str) -> bool:
-    pattern = r"^[\w.#$%&_](?=.*\d)(?=.{8,26}$)"
-    if re.match(pattern, password) is not None:
-        return True
-    else:
-        return False
+    pattern = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,32}$"
+    return re.match(pattern, password) is not None
