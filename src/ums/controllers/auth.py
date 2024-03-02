be900b12553da6e4fc422c9344adf85de88bd3b4
JKearnsl <boyarshinovn@gmail.com>
2024-03-03 00:52:45 +0300

    Added cookie setting logic

--- src/ums/controllers/auth.py
+++ src/ums/controllers/auth.py
@@ -1,13 +1,17 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from fastapi.requests import Request
 from fastapi.responses import Response
 from fastapi import status as http_status
 from pydantic import EmailStr
 
+from ums.config import JWTConfig
+from ums.dependencies.config import get_jwt_config
 from ums.views import UserResponse
 from ums.dependencies.services import get_services
 from ums.models import schemas
-from ums import ServiceFactory
+from ums.services import ServiceFactory
 
 router = APIRouter()
 
@@ -23,13 +27,47 @@ async def sign_up(data: schemas.UserCreate, services: ServiceFactory = Depends(g
 
 
 @router.post("/signIn", response_model=UserResponse, status_code=http_status.HTTP_200_OK)
-async def sign_in(user: schemas.UserAuth, response: Response, services: ServiceFactory = Depends(get_services)):
+async def sign_in(
+        user: schemas.UserAuth,
+        response: Response,
+        services: Annotated[ServiceFactory, Depends(get_services)],
+        config: Annotated[JWTConfig, Depends(get_jwt_config)]
+):
     """
     Вход в систему
 
     Требуемые права доступа: AUTHENTICATE
     """
-    return UserResponse(content=await services.auth.authenticate(user, response))
+    content, jwt_tokens, session_id = await services.auth.authenticate(user)
+    response.set_cookie(
+        key="access_token",
+        value=jwt_tokens[0],
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.ACCESS_EXP_SEC,
+        path="/api"
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=jwt_tokens[1],
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.REFRESH_EXP_SEC,
+        path="/api",
+    )
+    response.set_cookie(
+        key="session_id",
+        value=session_id,
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.REFRESH_EXP_SEC,
+        path="/api"
+    )
+
+    return UserResponse(content=content)
 
 
 @router.post('/logout', response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
@@ -39,18 +77,85 @@ async def logout(request: Request, response: Response, services: ServiceFactory 
 
     Требуемые права доступа: LOGOUT
     """
-    await services.auth.logout(request, response)
+    response.set_cookie(
+        key="access_token",
+        value="",
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=1,
+        path="/api"
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value="",
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=1,
+        path="/api",
+    )
+    response.set_cookie(
+        key="session_id",
+        value="",
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=1,
+        path="/api"
+    )
+    session_id = request.cookies.get("session_id")
+    await services.auth.logout(session_id)
 
 
 @router.post('/refresh_tokens', response_model=UserResponse, status_code=http_status.HTTP_200_OK)
-async def refresh(request: Request, response: Response, services: ServiceFactory = Depends(get_services)):
+async def refresh(
+        request: Request,
+        response: Response,
+        services: Annotated[ServiceFactory, Depends(get_services)],
+        config: Annotated[JWTConfig, Depends(get_jwt_config)]
+):
     """
     Обновить токены jwt
 
     Требуемые права доступа: None
     Состояние: ACTIVE
     """
-    return UserResponse(content=await services.auth.refresh_tokens(request, response))
+    jwt_tokens = (
+        request.cookies.get("access_token"),
+        request.cookies.get("refresh_token")
+    )
+    session_id = request.cookies.get("session_id")
+
+    content, new_jwt_tokens, new_session_id = await services.auth.refresh_tokens(jwt_tokens, session_id)
+    response.set_cookie(
+        key="access_token",
+        value=new_jwt_tokens[0],
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.ACCESS_EXP_SEC,
+        path="/api"
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=new_jwt_tokens[1],
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.REFRESH_EXP_SEC,
+        path="/api",
+    )
+    response.set_cookie(
+        key="session_id",
+        value=new_session_id,
+        secure=True,
+        httponly=True,
+        samesite="none",
+        max_age=config.REFRESH_EXP_SEC,
+        path="/api"
+    )
+    return UserResponse(content=content)
 
 
 @router.post("/send/{email}", response_model=None, status_code=http_status.HTTP_204_NO_CONTENT)
