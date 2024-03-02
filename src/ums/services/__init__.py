63a76a085f431ec96c4c7ea23175a9dcc66277a2
JKearnsl <boyarshinovn@gmail.com>
2024-03-03 00:46:55 +0300

    Req updated

--- src/ums/services/__init__.py
+++ src/ums/services/__init__.py
@@ -1,13 +1,18 @@
 from typing import AsyncGenerator, Callable
 
-from ums import Config
+from ums.config import Config
 from ums.models.auth import BaseUser
-from ums.services.auth import AuthApplicationService, JWTManager, SessionManager, ConfirmCodeUtil
-from ums.services.repository import RepoFactory
+from ums.security.jwt import JwtTokenProcessor
+from ums.security.session import SessionManager
+from ums.security.confirm_manager import ConfirmManager
+from ums.repositories import RepoFactory
+
+from ums.services.auth import AuthApplicationService
 from ums.services.role import RoleApplicationService
 from ums.services.stats import StatsApplicationService
-from ums import UserApplicationService
-from ums import EmailSender, S3Storage
+from ums.services.user import UserApplicationService
+
+from ums.utils import EmailSender, S3Storage
 
 
 class ServiceFactory:
@@ -17,9 +22,10 @@ class ServiceFactory:
             *,
             current_user: BaseUser,
             config: Config,
-            redis_sessions,
             redis_reauth,
-            redis_confirmations,
+            confirm_manager: ConfirmManager,
+            session_manager: SessionManager,
+            jwt: JwtTokenProcessor,
             email_sender: EmailSender,
             file_storage: S3Storage,
             lazy_session: Callable[[], AsyncGenerator],
@@ -27,9 +33,10 @@ class ServiceFactory:
         self._repo = repo_factory
         self._current_user = current_user
         self._config = config
-        self._redis_sessions = redis_sessions
+        self.session_manager = session_manager
         self._redis_reauth = redis_reauth
-        self._redis_confirmations = redis_confirmations
+        self.confirm_manager = confirm_manager
+        self.jwt = jwt
         self._email_sender = email_sender
         self._file_storage = file_storage
         self._lazy_session = lazy_session
@@ -38,12 +45,11 @@ class ServiceFactory:
     def auth(self) -> AuthApplicationService:
         return AuthApplicationService(
             self._current_user,
-            jwt_manager=JWTManager(config=self._config),
-            session_manager=SessionManager(redis_client=self._redis_sessions, config=self._config),
+            jwt=self.jwt,
+            session_manager=self.session_manager,
             user_repo=self._repo.user,
-            redis_client=self._redis_sessions,
             redis_client_reauth=self._redis_reauth,
-            confirm_code_util=ConfirmCodeUtil(redis=self._redis_confirmations),
+            confirm_manager=self.confirm_manager,
             email=self._email_sender
         )
 
@@ -55,14 +61,14 @@ class ServiceFactory:
             role_repo=self._repo.role,
             email=self._email_sender,
             redis_client_reauth=self._redis_reauth,
-            session=SessionManager(redis_client=self._redis_sessions, config=self._config),
-            config=self._config,
+            session=self.session_manager,
+            config=self._config.JWT,
             s3_storage=self._file_storage,
         )
 
     @property
     def stats(self) -> StatsApplicationService:
-        return StatsApplicationService(redis_client=self._redis_sessions, config=self._config)
+        return StatsApplicationService(config=self._config)
 
     @property
     def role(self) -> RoleApplicationService:
