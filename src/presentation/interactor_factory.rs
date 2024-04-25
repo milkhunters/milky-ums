9b05fc5ec2b2681b9505429ac0efa36696289780
JKearnsl <boyarshinovn@gmail.com>
2024-04-25 16:33:07 +0300

    added user interacts

--- src/presentation/interactor_factory.rs
+++ src/presentation/interactor_factory.rs
@@ -1,7 +1,12 @@
 
 use crate::application::user::get_by_id::GetUserById;
+use crate::application::user::get_range::GetUserRange;
+use crate::application::session::get_by_id::GetSessionById;
+use crate::application::session::get_by_user_id::GetSessionByUserId;
 
 pub trait InteractorFactory {
     fn get_user_by_id(&self) -> GetUserById;
-
+    fn get_user_range(&self) -> GetUserRange;
+    fn get_session_by_id(&self) -> GetSessionById;
+    fn get_sessions_by_user_id(&self) -> GetSessionByUserId;
 }
