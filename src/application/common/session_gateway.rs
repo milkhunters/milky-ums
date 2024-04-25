ae4a5f2c875a69dec857bed2ae6a6f8abb93c56a
JKearnsl <boyarshinovn@gmail.com>
2024-04-25 16:36:20 +0300

    updated

--- src/application/common/session_gateway.rs
+++ src/application/common/session_gateway.rs
@@ -1,7 +1,15 @@
 use uuid::Uuid;
+use async_trait::async_trait;
 use crate::domain::models::session::Session;
 
+#[async_trait]
 pub trait SessionReader {
     async fn get_session_by_id(&self, session_id: Uuid) -> Result<Session, String>;
+    async fn get_sessions_by_user_id(&self, user_id: Uuid) -> Result<Vec<Session>, String>;
 
+}
+#[async_trait]
+pub trait SessionWriter {
+    async fn create_session(&self, session: Session) -> Result<(), String>;
+    async fn delete_session(&self, session_id: Uuid) -> Result<(), String>;
 }
--- src/application/common/user_gateway.rs
+++ src/application/common/user_gateway.rs
@@ -1,8 +1,11 @@
+use async_trait::async_trait;
 use uuid::Uuid;
 use crate::domain::models::user;
 
+#[async_trait]
 pub trait UserReader {
     async fn get_user_by_id(&self, user_id: Uuid) -> Result<user::Model, String>;
     async fn get_user_by_username(&self, username: String) -> Result<user::Model, String>;
     async fn get_user_by_email(&self, email: String) -> Result<user::Model, String>;
+    async fn get_list(&self) -> Result<Vec<user::Model>, String>;
 }
