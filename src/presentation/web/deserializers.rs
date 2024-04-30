b147da7a5c47a4e1ac0738d3719c1837e913022f
JKearnsl <boyarshinovn@gmail.com>
2024-04-30 19:22:20 +0300

    bugfix

--- src/presentation/web/deserializers.rs
+++ src/presentation/web/deserializers.rs
@@ -3,14 +3,14 @@ use std::fmt::Write;
 use serde::de;
 use uuid::Uuid;
 
-pub fn deserialize_uuid_list<'de, D>(deserializer: D) -> Result<Vec<Uuid>, D::Error>
+pub fn deserialize_uuid_list<'de, D>(deserializer: D) -> Result<Option<Vec<Uuid>>, D::Error>
     where
         D: de::Deserializer<'de>,
 {
     struct StringVecVisitor;
 
     impl<'de> de::Visitor<'de> for StringVecVisitor {
-        type Value = Vec<Uuid>;
+        type Value = Option<Vec<Uuid>>;
 
         fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
             formatter.write_str("a string containing a list of UUIDs")
@@ -25,7 +25,14 @@ pub fn deserialize_uuid_list<'de, D>(deserializer: D) -> Result<Vec<Uuid>, D::Er
                 let id = Uuid::parse_str(id).map_err(E::custom)?;
                 ids.push(id);
             }
-            Ok(ids)
+            Ok(Option::from(ids))
+        }
+
+        fn visit_none<E>(self) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+        {
+            Ok(None)
         }
     }
 
