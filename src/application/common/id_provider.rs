57a9bacb23d583982c6c02095b0acf5e2c7a3feb
JKearnsl <boyarshinovn@gmail.com>
2024-05-12 19:34:05 +0300

    updated

--- src/application/common/id_provider.rs
+++ src/application/common/id_provider.rs
@@ -1,6 +1,11 @@
 use uuid::Uuid;
+use crate::domain::models::user::UserState;
 
 pub trait IdProvider {
     fn user_id(&self) -> Uuid;
-    fn permission(&self) -> String;
+    fn user_state(&self) -> UserState;
+    fn permissions(&self) -> Vec<String>;
+    fn user_agent(&self) -> String;
+    fn ip(&self) -> String;
+    fn is_auth(&self) -> bool;
 }
