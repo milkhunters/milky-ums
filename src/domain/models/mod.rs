2b8d7001e563d21845e07ab782c452eb98aa1fa1
JKearnsl <boyarshinovn@gmail.com>
2024-05-12 19:33:40 +0300

    added Permission type

--- src/domain/models/mod.rs
+++ src/domain/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod user;
 pub mod session;
 pub mod current_user;
+pub mod permission;
---
+++ src/domain/models/permission.rs
@@ -0,0 +1,24 @@
+
+
+
+pub enum Permission {
+    GetUserSelf,
+    GetUser,
+    CreateUser,
+    UpdateUser,
+    UpdateUserSelf,
+    DeleteUser,
+
+    GetSessionSelf,
+    GetSession,
+    CreateSession,
+    UpdateSession,
+    DeleteSession,
+    DeleteSessionSelf,
+
+    GetRoleSelf,
+    GetRole,
+    CreateRole,
+    UpdateRole,
+    DeleteRole,
+}
