cd4955a78c23f87602b53971adb026dc80de16eb
JKearnsl <boyarshinovn@gmail.com>
2024-05-12 19:34:32 +0300

    added UserWriter

--- src/application/common/user_gateway.rs
+++ src/application/common/user_gateway.rs
@@ -8,7 +8,13 @@ pub trait UserReader {
     async fn get_user_by_id(&self, user_id: Uuid) -> Option<UserDomain>;
     async fn get_users_by_ids(&self, user_ids: Vec<Uuid>) -> Option<Vec<UserDomain>>;
     async fn get_users_list(&self, limit: u64, offset: u64) -> Vec<UserDomain>;
-    async fn save_user(&self, data: &UserDomain);
     async fn get_user_by_username_not_sensitive(&self, username: String) -> Option<UserDomain>;
     async fn get_user_by_email_not_sensitive(&self, email: String) -> Option<UserDomain>;
+
+}
+
+#[async_trait]
+pub trait UserWriter {
+    async fn save_user(&self, data: &UserDomain);
+
 }
