aeb31104358cd79d4f6122b4ef9744ddb0fb15ea
JKearnsl <boyarshinovn@gmail.com>
2024-05-12 19:34:22 +0300

    updated

--- src/application/common/session_gateway.rs
+++ src/application/common/session_gateway.rs
@@ -1,16 +1,16 @@
-use uuid::Uuid;
 use async_trait::async_trait;
-use crate::application::common::exceptions::ApplicationError;
+use redis::geo::Unit;
+use uuid::Uuid;
+
 use crate::domain::models::session::Session;
 
 #[async_trait]
 pub trait SessionReader {
-    async fn get_session_by_id(&self, session_id: Uuid) -> Result<Session, ApplicationError>;
-    async fn get_sessions_by_user_id(&self, user_id: Uuid) -> Result<Vec<Session>, ApplicationError>;
-
+    async fn get_session_by_id(&self, session_id: Uuid) -> Option<Session>;
+    async fn get_sessions_by_user_id(&self, user_id: Uuid) -> Vec<Session>;
 }
 #[async_trait]
 pub trait SessionWriter {
-    async fn create_session(&self, session: Session) -> Result<(), ApplicationError>;
-    async fn delete_session(&self, session_id: Uuid) -> Result<(), ApplicationError>;
+    async fn save_session(&self, session: &Session) -> Unit;
+    async fn delete_session(&self, session_id: Uuid) -> Unit;
 }
