8c1643616dda686a2cec999cf06a5c114c2cc397
JKearnsl <boyarshinovn@gmail.com>
2024-05-18 19:22:22 +0300

    updated

--- src/domain/models/session.rs
+++ src/domain/models/session.rs
@@ -8,5 +8,5 @@ pub struct Session {
     pub ip: String,
     pub user_agent: String,
     pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
+    pub updated_at: Option<DateTime<Utc>>,
 }
