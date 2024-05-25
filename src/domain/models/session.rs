12fc4759062473c47727a17e3890af9588756230
JKearnsl <boyarshinovn@gmail.com>
2024-05-25 15:36:29 +0300

    added SessionId type

--- src/domain/models/session.rs
+++ src/domain/models/session.rs
@@ -1,10 +1,13 @@
 use chrono::{DateTime, Utc};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
+pub type SessionId = String;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Session {
-    pub id: String,
+    pub id: SessionId,
+    pub user_id: Uuid,
     pub ip: String,
     pub user_agent: String,
     pub created_at: DateTime<Utc>,
