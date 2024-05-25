bc855249ae5e13aba3575b44baab6137cd0bc489
JKearnsl <boyarshinovn@gmail.com>
2024-05-25 15:37:08 +0300

    updated create_session; added verify_session

--- src/domain/services/session.rs
+++ src/domain/services/session.rs
@@ -1,7 +1,8 @@
 use uuid::Uuid;
+use rand::random;
 
 use crate::application::common::exceptions::ApplicationError;
-use crate::domain::models::session::Session;
+use crate::domain::models::session::{Session, SessionId};
 
 pub struct SessionService { }
 
@@ -9,16 +10,30 @@ impl SessionService {
 
     pub fn create_session(
         &self,
+        user_id: Uuid,
         ip: String,
         user_agent: String,
     ) -> Result<Session, ApplicationError> {
+        let id: SessionId = SessionId::from(
+            (0..128).map(|_| format!("{:02x}", random::<u8>())).collect()
+        );
+        
         Ok(Session {
-            id: Uuid::new_v4().to_string(),
+            id,
+            user_id,
             ip,
             user_agent,
             created_at: chrono::Utc::now(),
-            updated_at: chrono::Utc::now(),
+            updated_at: None,
         })
+    }
+
+    pub fn verify_session(
+        &self,
+        session: Session,
+        user_agent: String,
+    ) -> Result<bool, ApplicationError> {
+        Ok(session.user_agent == user_agent)
     }
 
     pub fn update_session(
@@ -30,7 +45,7 @@ impl SessionService {
         Ok(Session {
             ip: new_ip,
             user_agent: new_user_agent,
-            updated_at: chrono::Utc::now(),
+            updated_at: Some(chrono::Utc::now()),
             ..session
         })
     }
