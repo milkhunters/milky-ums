7411ce735a61c2b75ffa49dcb09c3fd6172caaab
JKearnsl <boyarshinovn@gmail.com>
2024-05-25 15:36:03 +0300

    updated types

--- src/application/common/id_provider.rs
+++ src/application/common/id_provider.rs
@@ -1,11 +1,13 @@
 use uuid::Uuid;
+use crate::domain::models::session::SessionId;
 use crate::domain::models::user::UserState;
 
 pub trait IdProvider {
-    fn user_id(&self) -> Uuid;
-    fn user_state(&self) -> UserState;
-    fn permissions(&self) -> Vec<String>;
-    fn user_agent(&self) -> String;
-    fn ip(&self) -> String;
-    fn is_auth(&self) -> bool;
+    fn session_id(&self) -> Option<&SessionId>;
+    fn user_id(&self) -> Option<&Uuid>;
+    fn user_state(&self) -> Option<&UserState>;
+    fn permissions(&self) -> &Vec<String>;
+    fn user_agent(&self) -> &str;
+    fn ip(&self) -> &str;
+    fn is_auth(&self) -> &bool;
 }
