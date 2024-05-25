6ce2f465338f48b74c992ae8f085f3fabebd88fb
JKearnsl <boyarshinovn@gmail.com>
2024-05-25 15:35:54 +0300

    updated types

--- src/application/common/session_gateway.rs
+++ src/application/common/session_gateway.rs
@@ -1,16 +1,16 @@
 use async_trait::async_trait;
-use redis::geo::Unit;
 use uuid::Uuid;
 
-use crate::domain::models::session::Session;
+use crate::domain::models::session::{Session, SessionId};
 
 #[async_trait]
 pub trait SessionReader {
-    async fn get_session_by_id(&self, session_id: Uuid) -> Option<Session>;
-    async fn get_sessions_by_user_id(&self, user_id: Uuid) -> Vec<Session>;
+    async fn get_session(&self, session_id: &SessionId) -> Option<Session>;
+    async fn get_sessions(&self, user_id: &Uuid) -> Vec<Session>;
 }
+
 #[async_trait]
 pub trait SessionWriter {
-    async fn save_session(&self, session: &Session) -> Unit;
-    async fn delete_session(&self, session_id: Uuid) -> Unit;
+    async fn save_session(&self, data: &Session);
+    async fn delete_session(&self, session_id: &SessionId);
 }
