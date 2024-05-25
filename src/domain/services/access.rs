f58e71687032bdfe90bea4736c1059b592f2baf9
JKearnsl <boyarshinovn@gmail.com>
2024-05-25 15:34:54 +0300

    updated user and session access rules

--- src/domain/services/access.rs
+++ src/domain/services/access.rs
@@ -1,105 +1,299 @@
 use uuid::Uuid;
 use crate::domain::exceptions::DomainError;
-use crate::domain::models::permission::Permission;
+use crate::domain::models::session::SessionId;
+use crate::domain::models::ums_permission::UMSPermission;
 use crate::domain::models::user::UserState;
 
 pub struct AccessService {}
 
 impl AccessService {
-
-    pub fn ensure_can_get_user_self(
+    
+    pub fn ensure_can_create_user(
         &self,
-        user_id: &Uuid,
-        update_user_id: &Uuid,
-        user_state: &UserState,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
-        if !(
-            permissions.contains(Permission::GetUserSelf.as_str()) &&
-            user_id == update_user_id &&
-            user_state == &UserState::Active
-        ) {
+        if !permissions.contains(UMSPermission::CreateUser.as_str()) {
             return Err(DomainError::AccessDenied)
         }
         Ok(())
+    }
+
+    pub fn ensure_can_get_user_self(
+        &self,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+        
+        if 
+            permissions.contains(UMSPermission::GetUserSelf.as_str()) &&
+            user_state.unwrap() != &UserState::Inactive
+        {
+            return Ok(())
+        }
+        Err(DomainError::AccessDenied)
     }
 
     pub fn ensure_can_get_user(
         &self,
-        user_state: &UserState,
+        is_auth: &bool,
+        user_id: Option<&Uuid>,
+        get_user_id: &Uuid,
+        user_state: Option<&UserState>,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
-        if !(
-            permissions.contains(Permission::GetUser.as_str()) &&
-            user_state == &UserState::Active
-        ) {
-            return Err(DomainError::AccessDenied)
+        
+        if permissions.contains(UMSPermission::GetUser.as_str()) {
+            return Ok(())
         }
-        Ok(())
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+        
+        if 
+            permissions.contains(UMSPermission::GetUserSelf.as_str()) &&
+            user_id.unwrap() == get_user_id &&
+            user_state.unwrap() == &UserState::Active
+        {
+            return Ok(())
+        }
+        Err(DomainError::AccessDenied)
     }
+
+    pub fn ensure_can_get_users(
+        &self,
+        is_auth: &bool,
+        user_id: Option<&Uuid>,
+        get_user_ids: &Vec<Uuid>,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+
+        if permissions.contains(UMSPermission::GetUser.as_str()) {
+            return Ok(())
+        }
+
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+
+        if
+            permissions.contains(UMSPermission::GetUserSelf.as_str()) && 
+            get_user_ids.len() == 1 &&
+            get_user_ids.contains(&user_id.unwrap()) &&
+            user_state.unwrap() == &UserState::Active
+        {
+            return Ok(())
+        }
+        Err(DomainError::AccessDenied)
+    }
+
+    pub fn ensure_can_get_user_range(
+        &self,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+
+        if permissions.contains(UMSPermission::GetUser.as_str()) {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
+    }
+
 
     pub fn ensure_can_update_user(
         &self,
-        user_state: &UserState,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
-        if !(
-            permissions.contains(Permission::UpdateUser.as_str()) &&
-            user_state == &UserState::Active
-        ) {
-            return Err(DomainError::AccessDenied)
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
         }
-        Ok(())
+        
+        if 
+            user_state.unwrap() == &UserState::Active && 
+            permissions.contains(UMSPermission::UpdateUser.as_str())
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
     }
 
     pub fn ensure_can_update_user_self(
         &self,
-        user_id: &Uuid,
-        update_user_id: &Uuid,
-        user_state: &UserState,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
 
-        if !(
-            permissions.contains(Permission::UpdateUserSelf.as_str()) &&
-            user_id == update_user_id &&
-            user_state == &UserState::Active
-        ) {
-            return Err(DomainError::AccessDenied)
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
         }
-        Ok(())
+        
+        if
+            user_state.unwrap() == &UserState::Active &&
+            permissions.contains(UMSPermission::UpdateUserSelf.as_str())
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
     }
 
-    pub fn ensure_can_delete_session_by_id(
+    pub fn ensure_can_delete_session(
         &self,
-        user_session_id: &Uuid,
-        del_session_id: &Uuid,
-        user_state: &UserState,
+        is_auth: &bool,
+        user_session_id: Option<&SessionId>,
+        del_session_id: &SessionId,
+        user_state: Option<&UserState>,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
-        if !(
-            permissions.contains(Permission::DeleteSession.as_str()) &&
-            user_session_id == del_session_id &&
-            user_state == &UserState::Active
-        ) {
-            return Err(DomainError::AccessDenied)
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
         }
-        Ok(())
+        
+        if
+            permissions.contains(UMSPermission::DeleteSession.as_str()) ||
+            (
+                permissions.contains(UMSPermission::DeleteSessionSelf.as_str()) &&
+                user_session_id.unwrap() == del_session_id &&
+                user_state != &UserState::Inactive
+            )
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
     }
 
     pub fn ensure_can_delete_session_self(
         &self,
-        user_session_id: &Uuid,
-        user_state: &UserState,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
         permissions: &Vec<String>
     ) -> Result<(), DomainError> {
-        if !(
-            permissions.contains(Permission::DeleteSessionSelf.as_str()) &&
-            user_session_id == user_session_id &&
-            user_state == &UserState::Active
-        ) {
-            return Err(DomainError::AccessDenied)
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
         }
-        Ok(())
+        
+        if
+            user_state.unwrap() == &UserState::Active &&
+            permissions.contains(UMSPermission::DeleteSessionSelf.as_str())
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
     }
+    
+    pub fn ensure_can_create_session(
+        &self,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+            
+        if 
+            is_auth &&
+            permissions.contains(UMSPermission::CreateSession.as_str()) && 
+            user_state.unwrap() != UserState::Inactive 
+        {
+            return Ok(())
+        }
+        
+        if
+            !is_auth &&
+            permissions.contains(UMSPermission::CreateSession.as_str())
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
+    }
+    
+    pub fn ensure_can_get_session(
+        &self,
+        is_auth: &bool,
+        user_session_id: Option<&SessionId>,
+        get_session_id: &SessionId,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+        
+        if
+            permissions.contains(UMSPermission::GetSession.as_str()) ||
+            (
+                permissions.contains(UMSPermission::GetSessionSelf.as_str()) &&
+                user_session_id.unwrap() == get_session_id &&
+                user_state.unwrap() != &UserState::Inactive
+            )
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
+    }
+
+    pub fn ensure_can_get_sessions(
+        &self,
+        is_auth: &bool,
+        user_id: Option<&Uuid>,
+        get_user_id: &Uuid,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+        
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+
+        if permissions.contains(UMSPermission::GetSession.as_str()) {
+            return Ok(())
+        }
+        
+        if
+            permissions.contains(UMSPermission::GetSessionSelf.as_str()) &&
+            get_user_id == user_id.unwrap() &&
+            user_state.unwrap() == &UserState::Active
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
+    }
+    
+
+    pub fn ensure_can_get_session_self(
+        &self,
+        is_auth: &bool,
+        user_state: Option<&UserState>,
+        permissions: &Vec<String>
+    ) -> Result<(), DomainError> {
+        if !is_auth {
+            return Err(DomainError::AuthorizationRequired)
+        }
+        
+        if 
+            permissions.contains(UMSPermission::GetSessionSelf.as_str()) &&
+            user_state.unwrap() != &UserState::Inactive
+        {
+            return Ok(())
+        }
+        
+        Err(DomainError::AccessDenied)
+    }
+    
 }
