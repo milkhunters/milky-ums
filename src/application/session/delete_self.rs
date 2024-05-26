3714af9e76b12e1517073f7a737c79ad6216cb6a
JKearnsl <boyarshinovn@gmail.com>
2024-05-26 14:13:28 +0300

    removed unused deps

--- src/application/session/delete_self.rs
+++ src/application/session/delete_self.rs
@@ -2,15 +2,12 @@ use crate::application::common::exceptions::{ApplicationError, ErrorContent};
 use crate::application::common::id_provider::IdProvider;
 use crate::application::common::interactor::Interactor;
 use crate::application::common::session_gateway::SessionWriter;
-use crate::application::common::user_gateway::UserReader;
 use crate::domain::exceptions::DomainError;
 use crate::domain::services::access::AccessService;
-use crate::domain::services::user::UserService;
+
 
 pub struct DeleteSessionSelf<'a> {
     pub session_writer: &'a dyn SessionWriter,
-    pub user_gateway: &'a dyn UserReader,
-    pub user_service: &'a UserService,
     pub id_provider: &'a dyn IdProvider,
     pub access_service: &'a AccessService,
 }
@@ -38,7 +35,7 @@ impl Interactor<(), ()> for DeleteSessionSelf<'_> {
             }
         };
         
-        self.session_writer.delete_session(&self.id_provider.session_id().unwrap()).await?;
+        self.session_writer.delete_session(&self.id_provider.session_id().unwrap()).await;
 
         Ok(())
     }
