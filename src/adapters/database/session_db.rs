2bec65f4f2cc74dab5abde4675fad30b3c990f63
JKearnsl <boyarshinovn@gmail.com>
2024-05-26 14:08:53 +0300

    added SessionGatewayTrait

--- src/adapters/database/session_db.rs
+++ src/adapters/database/session_db.rs
@@ -1,11 +1,10 @@
 use async_trait::async_trait;
 use deadpool_redis::Pool;
 use redis::cmd;
-use redis::geo::Unit;
 use uuid::Uuid;
 
-use crate::application::common::session_gateway::{SessionReader, SessionWriter};
-use crate::domain::models::session::Session;
+use crate::application::common::session_gateway::{SessionReader, SessionWriter, SessionGateway as SessionGatewayTrait};
+use crate::domain::models::session::{Session, SessionId};
 
 pub struct SessionGateway {
     pub redis_pool: Box<Pool>
@@ -19,7 +18,7 @@ impl SessionGateway {
 
 #[async_trait]
 impl SessionReader for SessionGateway {
-    async fn get_session_by_id(&self, session_id: Uuid) -> Option<Session>{
+    async fn get_session(&self, session_id: &SessionId) -> Option<Session> {
         let mut conn = self.redis_pool.get().await.unwrap();
         match cmd("GET")
             .arg(&["deadpool/test_key"])
@@ -28,10 +27,11 @@ impl SessionReader for SessionGateway {
             Ok(value) => {
                 Some(Session {
                     id: session_id.to_string(),
+                    user_id: Uuid::new_v4(),
                     ip: "some ip".to_string(),
                     user_agent: "some user agent".to_string(),
                     created_at: chrono::Utc::now(),
-                    updated_at: chrono::Utc::now(),
+                    updated_at: Some(chrono::Utc::now()),
                 })
             },
             Err(e) => {
@@ -40,18 +40,46 @@ impl SessionReader for SessionGateway {
         }
     }
 
-    async fn get_sessions_by_user_id(&self, user_id: Uuid) -> Vec<Session> {
-        todo!()
+    async fn get_sessions(&self, user_id: &Uuid) -> Vec<Session> {
+        let mut conn = self.redis_pool.get().await.unwrap();
+        match cmd("GET")
+            .arg(&["deadpool/test_key"])
+            .query_async::<_, String>(&mut conn)
+            .await {
+            Ok(value) => {
+                vec![Session {
+                    id: Uuid::new_v4().to_string(),
+                    user_id: user_id.clone(),
+                    ip: "some ip".to_string(),
+                    user_agent: "some user agent".to_string(),
+                    created_at: chrono::Utc::now(),
+                    updated_at: Some(chrono::Utc::now()),
+                }]
+            },
+            Err(e) => {
+                vec![]
+            }
+        }
     }
 }
 
 #[async_trait]
 impl SessionWriter for SessionGateway {
-    async fn save_session(&self, session: &Session) -> Unit {
-        todo!()
+    async fn save_session(&self, data: &Session) {
+        let mut conn = self.redis_pool.get().await.unwrap();
+        let _: () = cmd("SET")
+            .arg(&[data.id.as_str(), serde_json::to_string(data).unwrap().as_str()])
+            .query_async(&mut conn)
+            .await.unwrap();
     }
 
-    async fn delete_session(&self, session_id: Uuid) -> Unit {
-        todo!()
+    async fn delete_session(&self, session_id: &SessionId) {
+        let mut conn = self.redis_pool.get().await.unwrap();
+        let _: () = cmd("DEL")
+            .arg(&[session_id])
+            .query_async(&mut conn)
+            .await.unwrap();
     }
 }
+
+impl SessionGatewayTrait for SessionGateway {}
