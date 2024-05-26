7a9bf3430eb98c1a39d80cd58cb8dd5ec37d3ed5
JKearnsl <boyarshinovn@gmail.com>
2024-05-26 16:54:15 +0300

    updated

--- src/domain/services/user.rs
+++ src/domain/services/user.rs
@@ -31,12 +31,16 @@ impl UserService {
     pub fn update_user(
         &self,
         user: User,
+        new_username: String,
         new_email: String,
+        new_state: UserState,
         new_first_name: Option<String>,
         new_last_name: Option<String>,
     ) -> Result<User, ApplicationError> {
         Ok(User {
+            username: new_username,
             email: new_email,
+            state: new_state,
             first_name: new_first_name,
             last_name: new_last_name,
             updated_at: Some(chrono::Utc::now()),
