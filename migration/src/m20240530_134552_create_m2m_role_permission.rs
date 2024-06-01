81cd7dec1fcfd5b7ee29eb9cf8a9f3c290945910
JKearnsl <boyarshinovn@gmail.com>
2024-06-01 11:47:21 +0300

    added m2m role-perm migration

---
+++ migration/src/m20240530_134552_create_m2m_role_permission.rs
@@ -0,0 +1,61 @@
+use sea_orm_migration::prelude::*;
+use crate::m20240530_130156_create_role::Roles;
+use crate::m20240530_133643_create_permission::Permissions;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(RolePermissions::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(RolePermissions::RoleId)
+                            .uuid()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(RolePermissions::PermissionId)
+                            .uuid()
+                            .not_null()
+                    )
+                    .primary_key(
+                        Index::create()
+                            .table(RolePermissions::Table)
+                            .col(RolePermissions::RoleId)
+                            .col(RolePermissions::PermissionId),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(RolePermissions::Table, RolePermissions::PermissionId)
+                            .to(Permissions::Table, Permissions::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(RolePermissions::Table, RolePermissions::RoleId)
+                            .to(Roles::Table, Roles::Id)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(RolePermissions::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum RolePermissions {
+    Table,
+    RoleId,
+    PermissionId,
+}
