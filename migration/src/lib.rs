6f546fa52caf29eec8edc846786eb99c20c80db4
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:56:27 +0300

    updated

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -8,6 +8,8 @@ mod m20240530_132130_create_service;
 mod m20240530_134552_create_m2m_role_permission;
 mod m20240530_141709_create_m2m_role_user;
 mod m20240601_100718_create_session;
+mod m20240607_103644_create_default_role_id_cell;
+mod m20240607_103716_create_app_init_state_cell;
 
 pub struct Migrator;
 
@@ -18,11 +20,13 @@ impl MigratorTrait for Migrator {
             Box::new(m20240411_194825_create_user_state_enum::Migration),
             Box::new(m20240412_063317_create_user::Migration),
             Box::new(m20240530_130156_create_role::Migration),
-            Box::new(m20240530_132130_create_service::Migration),
             Box::new(m20240530_133643_create_permission::Migration),
+            Box::new(m20240530_132130_create_service::Migration),
             Box::new(m20240530_134552_create_m2m_role_permission::Migration),
             Box::new(m20240530_141709_create_m2m_role_user::Migration),
             Box::new(m20240601_100718_create_session::Migration),
+            Box::new(m20240607_103644_create_default_role_id_cell::Migration),
+            Box::new(m20240607_103716_create_app_init_state_cell::Migration),
         ]
     }
 }
