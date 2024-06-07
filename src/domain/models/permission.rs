19029fd41b29d0504907dc01e5731de0adf79c42
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:51:33 +0300

    now permission service near with model

--- src/domain/models/permission.rs
+++ src/domain/models/permission.rs
@@ -1,15 +1,40 @@
-
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use crate::domain::models::service::ServiceId;
+
+pub type PermissionId = Uuid;
+
+pub type PermissionTextId = String;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Permission {
-    pub id: Uuid,
+    pub id: PermissionId,
+    pub text_id: PermissionTextId,
+    
+    pub service_id: ServiceId,
     pub title: String,
-    pub description: String,
-    pub permissions: Vec<String>,
+    pub description: Option<String>,
 
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
+}
+
+impl Permission {
+    pub fn new(
+        text_id: PermissionTextId,
+        service_id: ServiceId,
+        title: String,
+        description: Option<String>,
+    ) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            text_id,
+            service_id,
+            title,
+            description,
+            created_at: Utc::now(),
+            updated_at: None,
+        }
+    }
 }
