dc4841a95c2bd7c38699141121730b655a3b56c5
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:58:17 +0300

    updated

--- src/adapters/database/session_db.rs
+++ src/adapters/database/session_db.rs
@@ -1,97 +1,243 @@
+use std::str::FromStr;
+
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
 use deadpool_redis::Pool;
+use futures::future::select;
 use redis::cmd;
+use sea_orm::{ActiveEnum, DbBackend, DbConn, EntityOrSelect, EntityTrait, FromQueryResult, JoinType, JsonValue, ModelTrait, QueryFilter, QuerySelect, QueryTrait, Related, RelationTrait, SelectColumns, Statement};
+use sea_orm::ActiveValue::Set;
+use sea_orm::prelude::Expr;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::application::common::session_gateway::{SessionReader, SessionWriter, SessionGateway as SessionGatewayTrait};
-use crate::domain::models::session::{Session, SessionId};
+use crate::adapters::database::models::{permissions, role_permissions, role_user, roles, sessions, users};
+use crate::adapters::database::models::prelude::Users;
+use crate::adapters::database::models::services::Relation::Permissions;
+use crate::application::common::session_gateway::{SessionGateway as SessionGatewayTrait, SessionReader, SessionRemover, SessionWriter};
+use crate::domain::models::permission::PermissionTextId;
+use crate::domain::models::role::RoleId;
+use crate::domain::models::session::{
+    Session, 
+    SessionId,
+    SessionTokenHash
+};
+use crate::domain::models::user::UserState;
 
 pub struct SessionGateway {
-    pub redis_pool: Box<Pool>
+    cache_redis_pool: Box<Pool>,
+    db: Box<DbConn>,
 }
 
 impl SessionGateway {
-    pub fn new(redis_pool: Box<Pool>) -> Self {
-        SessionGateway { redis_pool }
+    pub fn new(
+        redis_pool: Box<Pool>,
+        db: Box<DbConn>
+    ) -> Self {
+        SessionGateway {
+            cache_redis_pool: redis_pool,
+            db
+        }
     }
 }
 
 #[async_trait]
 impl SessionReader for SessionGateway {
     async fn get_session(&self, session_id: &SessionId) -> Option<Session> {
-        let mut conn = self.redis_pool.get().await.unwrap();
+        match sessions::Entity::find_by_id(session_id.clone())
+            .one(&*self.db)
+            .await.unwrap() {
+            Some(model) => Some(map_session_model_to_domain(model)),
+            None => None
+        }
+    }
+
+    async fn get_session_by_token_hash(
+        &self,
+        token_hash: &SessionTokenHash
+    ) -> Option<(Session, UserState, Vec<(RoleId, Vec<PermissionTextId>)>)> {
+        // let session_vec= sessions::Entity::find()
+        //     .select_with(permissions::Entity)
+        //     .select_column(roles::Column::Id)
+        //     .filter(Expr::col(sessions::Column::TokenHash).eq(token_hash.as_str()))
+        //     .join(
+        //         JoinType::LeftJoin,
+        //         sessions::Relation::Users.def()
+        //     )
+        //     .join(
+        //         JoinType::LeftJoin,
+        //         users::Relation::RoleUser.def()
+        //     )
+        //     .join(
+        //         JoinType::LeftJoin,
+        //         role_user::Relation::Roles.def()
+        //     )
+        //     .join(
+        //         JoinType::LeftJoin,
+        //         roles::Relation::RolePermissions.def()
+        //     )
+        //     .join(
+        //         JoinType::LeftJoin,
+        //         role_permissions::Relation::Permissions.def()
+        //     )
+        //     .all(&*self.db)
+        //     .await.unwrap();
+        let session_vec= sessions::Entity::find()
+            .filter(Expr::col(sessions::Column::TokenHash).eq(token_hash.as_str()))
+            .all(&*self.db)
+            .await.unwrap();
+        
+        if session_vec.is_empty() {
+            return None;
+        }
+        
+        if session_vec.len() > 1 {
+            panic!("Multiple sessions with the same token hash");
+        }
+        
+        let session = session_vec.first().unwrap();
+        
+        let user = session.find_related(Users).one(&*self.db).await.unwrap().unwrap();
+        let user_state = UserState::from_str(user.state.to_value().as_str()).unwrap();
+        
+        let roles = user.find_related(roles::Entity).all(&*self.db).await.unwrap();
+        
+        let subquery = user.find_related(role_user::Entity).select_column(role_user::Column::RoleId).as_query();
+        let permissions = role_permissions::Entity::find()
+            .filter(Expr::col(role_permissions::Column::RoleId).in_subquery(
+                user.find_related(role_user::Entity)
+                    .select_column(role_user::Column::RoleId)
+                    .as_query().to_owned()
+            ))
+            .join(
+                JoinType::LeftJoin,
+                role_permissions::Relation::Permissions.def()
+            )
+            .join(
+                JoinType::LeftJoin,
+                role_permissions::Relation::Roles.def()
+            )
+            .all(&*self.db)
+            .await.unwrap();
+
+        let mut roles_permissions: Vec<(RoleId, Vec<PermissionTextId>)> = Vec::new();
+        None
+        
+
+    }
+
+    async fn get_session_by_token_hash_from_cache(
+        &self,
+        token_hash: &SessionTokenHash
+    ) -> Option<(Session, UserState, Vec<(RoleId, Vec<PermissionTextId>)>)> {
+        let mut conn = self.cache_redis_pool.get().await.unwrap();
         match cmd("GET")
-            .arg(session_id.as_str())
+            .arg(token_hash.as_str())
             .query_async::<_, String>(&mut conn)
             .await {
             Ok(value) => {
-                let model: Session = serde_json::from_str(value.as_str()).unwrap();
-                Some(model)
+                Some(
+                    serde_json::from_str::<(
+                        Session,
+                        UserState,
+                        Vec<(RoleId, Vec<PermissionTextId>)>
+                    )>(value.as_str()).unwrap()
+                )
             },
-            Err(e) => {
+            Err(_) => {
                 None
             }
         }
     }
 
-    async fn get_sessions(&self, user_id: &Uuid) -> Vec<Session> {
-        let mut conn = self.redis_pool.get().await.unwrap();
-        let values = cmd("SMEMBERS")
-            .arg(user_id.to_string().as_str())
-            .query_async::<_, Vec<SessionId>>(&mut conn)
-            .await.unwrap_or_else(|e| {
-            vec![]
-        });
-        
-        return match cmd("MGET")
-            .arg(values)
-            .query_async::<_, Vec<String>>(&mut conn)
-            .await {
-            Ok(values) => {
-                values.iter().map(|value| {
-                    let model: Session = serde_json::from_str(value.as_str()).unwrap();
-                    model
-                }).collect()
-            },
-            Err(e) => {
-                vec![]
-            }
-        };
+    async fn get_user_sessions(&self, user_id: &Uuid) -> Vec<Session> {
+        let sessions: Vec<sessions::Model> = sessions::Entity::find().filter(
+            Expr::col(sessions::Column::UserId).eq(user_id.to_string())
+        )
+            .all(&*self.db)
+            .await
+            .unwrap();
+
+        sessions.iter().map(
+            |model| map_session_model_to_domain(model.clone())
+        ).collect()
     }
 }
 
 #[async_trait]
 impl SessionWriter for SessionGateway {
     async fn save_session(&self, data: &Session) {
-        let mut conn = self.redis_pool.get().await.unwrap();
+        let session_model = sessions::ActiveModel {
+            id: Set(data.id),
+            token_hash: Set(data.token_hash.clone()),
+            user_id: Set(data.user_id),
+            ip: Set(data.ip.clone()),
+            user_agent: Set(data.user_agent.clone()),
+            created_at: Set(data.created_at),
+            updated_at: Set(data.updated_at.clone())
+        };
+
+        let mut conn = self.cache_redis_pool.get().await.unwrap();
+
+        cmd("SET")
+            .arg(data.token_hash.as_str())
+            .arg(serde_json::to_string(&data).unwrap().as_str())
+            .query_async::<_, ()>(&mut conn)
+            .await.unwrap();
         
-        // todo: gather all commands in a transaction
-        let _: () = cmd("SET")
-            .arg(data.id.as_str())
-            .arg(serde_json::to_string(data).unwrap().as_str())
-            .query_async(&mut conn)
-            .await.unwrap();
-        let _: () = cmd("SADD")
-            .arg(data.user_id.to_string().as_str())
-            .arg(data.id.as_str())
-            .query_async(&mut conn)
-            .await.unwrap();
+        match sessions::Entity::find_by_id(data.id).one(&*self.db).await.unwrap() {
+            Some(_) => {
+                sessions::Entity::update(session_model).exec(&*self.db).await.unwrap();
+            },
+            None => {
+                sessions::Entity::insert(session_model).exec(&*self.db).await.unwrap();
+            }
+        }
     }
 
-    async fn delete_session(&self, session_id: &SessionId, user_id: &Uuid) {
-        let mut conn = self.redis_pool.get().await.unwrap();
+    async fn save_session_to_cache(
+        &self,
+        data: &Session,
+        user_state: &UserState,
+        roles: &Vec<(RoleId, Vec<PermissionTextId>)>
+    ) {
+        let mut conn = self.cache_redis_pool.get().await.unwrap();
         
-        // todo: gather all commands in a transaction
-        let _: () = cmd("DEL")
-            .arg(session_id.as_str())
-            .query_async(&mut conn)
-            .await.unwrap();
-        let _: () = cmd("SREM")
-            .arg(user_id.to_string().as_str())
-            .arg(session_id.as_str())
-            .query_async(&mut conn)
+        let serde_json = serde_json::to_string(&(
+            data.clone(),
+            user_state.clone(),
+            roles.clone()
+        )).unwrap();
+        
+        cmd("SET")
+            .arg(data.token_hash.as_str())
+            .arg(serde_json.as_str())
+            .query_async::<_, ()>(&mut conn)
             .await.unwrap();
     }
 }
 
+#[async_trait]
+impl SessionRemover for SessionGateway {
+    async fn remove_session(&self, session_id: &SessionId) {
+        sessions::Entity::delete_by_id(session_id.clone())
+            .exec(&*self.db)
+            .await
+            .unwrap();
+    }
+    
+}
+
 impl SessionGatewayTrait for SessionGateway {}
+
+fn map_session_model_to_domain(model: sessions::Model) -> Session {
+    Session {
+        id: model.id,
+        token_hash: model.token_hash,
+        user_id: model.user_id,
+        ip: model.ip,
+        user_agent: model.user_agent,
+        created_at: model.created_at,
+        updated_at: model.updated_at,
+    }
+}
--- src/adapters/database/user_db.rs
+++ src/adapters/database/user_db.rs
@@ -8,8 +8,9 @@ use sea_orm::ActiveValue::Set;
 use sea_orm::sea_query::{Condition, Expr};
 use sea_orm::sea_query::extension::postgres::PgExpr;
 use uuid::Uuid;
+use crate::adapters::database::models::sea_orm_active_enums::UserState;
 
-use crate::adapters::database::models::user;
+use crate::adapters::database::models::users;
 use crate::application::common::user_gateway::{
     UserReader, 
     UserWriter, 
@@ -20,7 +21,7 @@ use crate::domain::models::user::UserState as UserStateDomain;
 
 pub struct UserGateway{
     pub db: Box<DbConn>,
-    cache_user_by_id: Arc<Mutex<TimedCache<Uuid, user::Model>>>,
+    cache_user_by_id: Arc<Mutex<TimedCache<Uuid, users::Model>>>,
 }
 
 impl UserGateway {
@@ -45,7 +46,7 @@ impl UserReader for UserGateway {
             }
         }
 
-        match user::Entity::find_by_id(user_id.clone()).one(&*self.db).await.unwrap() {
+        match users::Entity::find_by_id(user_id.clone()).one(&*self.db).await.unwrap() {
             Some(user) => {
                 self.cache_user_by_id.lock().unwrap().cache_set(user_id.clone(), user.clone());
                 Option::from(map_user_model_to_domain(user))
@@ -55,11 +56,11 @@ impl UserReader for UserGateway {
     }
 
     async fn get_users_by_ids(&self, user_ids: &Vec<Uuid>) -> Option<Vec<UserDomain>> {
-        let users: Vec<user::Model> = user::Entity::find().filter(
+        let users: Vec<users::Model> = users::Entity::find().filter(
             {
                 let mut condition = Condition::any();
                 for id in user_ids {
-                    condition = condition.add(Expr::col(user::Column::Id).eq(*id));
+                    condition = condition.add(Expr::col(users::Column::Id).eq(*id));
                 }
                 condition
             }
@@ -80,7 +81,7 @@ impl UserReader for UserGateway {
     }
 
     async fn get_users_list(&self, limit: &u64, offset: &u64) -> Vec<UserDomain> {
-        let users: Vec<user::Model> = user::Entity::find()
+        let users: Vec<users::Model> = users::Entity::find()
             .limit(limit.clone())
             .offset(offset.clone())
             .all(&*self.db)
@@ -90,8 +91,8 @@ impl UserReader for UserGateway {
     }
 
     async fn get_user_by_username_not_sensitive(&self, username: &String) -> Option<UserDomain> {
-        let user: Option<user::Model> = user::Entity::find().filter(
-                Expr::col(user::Column::Username).ilike(username)
+        let user: Option<users::Model> = users::Entity::find().filter(
+                Expr::col(users::Column::Username).ilike(username)
             )
             .one(&*self.db)
             .await
@@ -104,8 +105,8 @@ impl UserReader for UserGateway {
     }
 
     async fn get_user_by_email_not_sensitive(&self, email: &String) -> Option<UserDomain> {
-        let user: Option<user::Model> = user::Entity::find().filter(
-                Expr::col(user::Column::Email).ilike(email)
+        let user: Option<users::Model> = users::Entity::find().filter(
+                Expr::col(users::Column::Email).ilike(email)
             )
             .one(&*self.db)
             .await
@@ -121,29 +122,29 @@ impl UserReader for UserGateway {
 #[async_trait]
 impl UserWriter for UserGateway {
     async fn save_user(&self, data: &UserDomain) {
-        let user_model = user::ActiveModel {
+        let user_model = users::ActiveModel {
             id: Set(data.id),
             username: Set(data.username.clone()),
             email: Set(data.email.clone()),
             first_name: Set(data.first_name.clone()),
             last_name: Set(data.last_name.clone()),
             state: Set(match data.state {
-                UserStateDomain::Active => user::UserState::Active,
-                UserStateDomain::Inactive => user::UserState::Inactive,
-                UserStateDomain::Banned => user::UserState::Banned,
-                UserStateDomain::Deleted => user::UserState::Deleted
+                UserStateDomain::Active => UserState::Active,
+                UserStateDomain::Inactive => UserState::Inactive,
+                UserStateDomain::Banned => UserState::Banned,
+                UserStateDomain::Deleted => UserState::Deleted
             }),
             hashed_password: Set(data.hashed_password.clone()),
             created_at: Set(data.created_at),
             updated_at: Set(data.updated_at.clone())
         };
 
-        match user::Entity::find_by_id(data.id).one(&*self.db).await.unwrap() {
+        match users::Entity::find_by_id(data.id).one(&*self.db).await.unwrap() {
             Some(_) => {
-                user::Entity::update(user_model).exec(&*self.db).await.unwrap();
+                users::Entity::update(user_model).exec(&*self.db).await.unwrap();
             }
             None => {
-                user::Entity::insert(user_model).exec(&*self.db).await.unwrap();
+                users::Entity::insert(user_model).exec(&*self.db).await.unwrap();
             }
         }
     }
@@ -151,7 +152,7 @@ impl UserWriter for UserGateway {
 
 
 
-fn map_user_model_to_domain(user: user::Model) -> UserDomain {
+fn map_user_model_to_domain(user: users::Model) -> UserDomain {
     UserDomain {
         id: user.id,
         username: user.username,
@@ -159,10 +160,10 @@ fn map_user_model_to_domain(user: user::Model) -> UserDomain {
         first_name: user.first_name,
         last_name: user.last_name,
         state: match user.state {
-            user::UserState::Active => UserStateDomain::Active,
-            user::UserState::Inactive => UserStateDomain::Inactive,
-            user::UserState::Banned => UserStateDomain::Banned,
-            user::UserState::Deleted => UserStateDomain::Deleted
+            UserState::Active => UserStateDomain::Active,
+            UserState::Inactive => UserStateDomain::Inactive,
+            UserState::Banned => UserStateDomain::Banned,
+            UserState::Deleted => UserStateDomain::Deleted
         },
         hashed_password: user.hashed_password,
         created_at: user.created_at,
