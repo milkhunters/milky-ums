8b8a0c5665abfd5a555c045c26ecce79803f35b8
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:52:06 +0300

    added RoleId type; removed permissions; fix description type

--- src/domain/models/role.rs
+++ src/domain/models/role.rs
@@ -2,13 +2,13 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+pub type RoleId = Uuid;
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Role {
-    pub id: Uuid,
+    pub id: RoleId,
     pub title: String,
-    pub description: String,
-    pub permissions: Vec<String>,
-
+    pub description: Option<String>,
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
 }
