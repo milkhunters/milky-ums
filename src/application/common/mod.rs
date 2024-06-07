4b3b626939d22b37c559b5ec29d675c591b357f4
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 15:18:10 +0300

    added init_state_gateway.rs

--- src/application/common/mod.rs
+++ src/application/common/mod.rs
@@ -4,6 +4,7 @@ pub mod role_gateway;
 pub mod session_gateway;
 pub mod permission_gateway;
 pub mod service_gateway;
+pub mod init_state_gateway;
 pub mod id_provider;
 pub mod exceptions;
 pub mod hasher;
