7dfa256e7a4c5010916b07a14dd362cb84b33cc4
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:55:59 +0300

    initial

---
+++ migration/src/m20240607_103716_create_app_init_state_cell.rs
@@ -0,0 +1,36 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(AppInitState::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(AppInitState::Id)
+                            .date_time()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(AppInitState::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum AppInitState {
+    Table,
+    Id,
+}
