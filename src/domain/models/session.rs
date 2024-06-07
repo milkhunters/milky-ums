ee44fbab3b59b9a2b1bf4612ed58eeb8f9e71b69
JKearnsl <boyarshinovn@gmail.com>
2024-06-07 13:52:41 +0300

    added token_hash; added new types SessionToken, SessionTokenHash

--- src/domain/models/session.rs
+++ src/domain/models/session.rs
@@ -2,14 +2,20 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-pub type SessionId = String;
+pub type SessionId = Uuid;
+pub type SessionToken = String;
+pub type SessionTokenHash = String;
 
-#[derive(Deserialize, Serialize)]
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Session {
     pub id: SessionId,
+    pub token_hash: SessionTokenHash,
     pub user_id: Uuid,
+    
     pub ip: String,
     pub user_agent: String,
+    
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
 }
